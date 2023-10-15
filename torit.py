db09a4c2d0361043ec8f04f2d39c17938ca4c2fc
Michael Lembck <lembckm@gmail.com>
2023-10-15 01:44:56 -0400

    Multiple Teachers and Classrooms Compatible

--- torit.py
+++ torit.py
@@ -1,3 +1,31 @@
+class School:
+    def __init__(self, teachers=[], classrooms=[]):
+        self.teachers = teachers
+        self.classrooms = classrooms
+
+    def hasInvalidSchedule(self):
+        always_staffed = all([classroom.is_staffed() for classroom in self.classrooms])
+        return not always_staffed
+
+    def teachersHaveNoAvailability(self):
+        return not any([teacher.has_availability for teacher in self.teachers])
+
+    def assignTeacherToClassroom(self, teacher, classroom, times):
+        for time in times:
+            teacher.assign_classroom(classroom.name, time)
+            classroom.assign_teacher(teacher.name, time)
+
+    def noSolutionPossible(self):
+        print("No Solution Possible")
+        print(self)
+
+    def complete(self):
+        print("Solution Found")
+        print(self)
+
+    def __str__(self):
+        return "\n".join([teacher.__str__() for teacher in self.teachers]) + "\n" + "\n".join([classroom.__str__() for classroom in self.classrooms])
+
 class Teacher:
     def __init__(self, name):
         self.name = name
@@ -9,6 +37,13 @@ class Teacher:
 
     def when_available(self):
         return [i for i in range(len(self.availability)) if self.availability[i]]
+
+    def assign_classroom(self, classroom, time):
+        self.availability[time] = False
+        self.placements[time] = classroom
+
+    def __str__(self):
+        return self.name + "'s schedule: " + ", ".join([str(p) for p in self.placements])
 
 class Classroom:
     def __init__(self, name):
@@ -22,43 +57,43 @@ class Classroom:
     def when_unstaffed(self):
         return [i for i in range(len(self.staffed)) if not self.staffed[i]]
 
-def checkValidity(teachers, classrooms):
-    always_staffed = all([classroom.is_staffed() for classroom in classrooms])
-    #one_place_at_once = 
-    return always_staffed
+    def assign_teacher(self, teacher, time):
+        self.staffed[time] = True
+        self.placements[time] = teacher
 
-def anyAvailability(teachers):
-    return any([teacher.has_availability for teacher in teachers])
+    def __str__(self):
+        return self.name + "'s schedule: " + ", ".join([str(p) for p in self.placements])
 
-def assignTeacherToClassroom(teacher, classroom, times):
-    for time in times:
-        teacher.availability[time] = False
-        teacher.placements[time] = classroom.name
-        classroom.staffed[time] = True
-        classroom.placements[time] = teacher.name
 
 def main():
-    teachers = [Teacher("Abby")]
-    classrooms = [Classroom("Infants")]
-
-    while not checkValidity(teachers, classrooms):
-        if not anyAvailability(teachers):
-            print("No solution possible")
+    teacher_names = ["Abby", "Beatrice", "Charlotte", "Danielle"]
+    classroom_names = ["Infants", "Toddlers", "Pre-school", "Elementary"]
+    torit = School([Teacher(name) for name in teacher_names], [Classroom(name) for name in classroom_names])
+    
+    while torit.hasInvalidSchedule():
+        if torit.teachersHaveNoAvailability():
+            torit.noSolutionPossible()
             break
-        for classroom in classrooms:
+        for classroom in torit.classrooms:
             if classroom.is_staffed():
                 continue
+            
             times_unstaffed = classroom.when_unstaffed()
-            for teacher in teachers:
+            
+            for teacher in torit.teachers:
                 if not teacher.has_availability():
                     continue
+                
                 times_available = teacher.when_available()
                 times_aligned = set(times_unstaffed).intersection(set(times_available))
-                assignTeacherToClassroom(teacher, classroom, times_aligned)
-                if checkValidity(teachers, classrooms):
-                    print("Solution found")
-                    print(teacher.placements)
-                    print(classroom.placements)
-                    return
+                torit.assignTeacherToClassroom(teacher, classroom, times_aligned)
 
-main()
+                if not torit.hasInvalidSchedule():
+                    return torit
+
+                if classroom.is_staffed():
+                    break
+                
+
+torit = main()
+torit.complete()
